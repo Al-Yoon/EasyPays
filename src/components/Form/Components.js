0ee0b6be231d13999a1cc789f69b6778425468bd
Al-Yoon <al.yoon10@gmail.com>
2024-09-20 20:08:04 -0300

    guardado v1

--- src/components/Form/Components.js
+++ src/components/Form/Components.js
@@ -114,8 +114,8 @@ ${props =>
 export const Overlay = styled.div`
 display:flexWrap;
 background: black;
-background: -webkit-linear-gradient(to right, black, black);
-background: linear-gradient(to right, black, black);
+background: -webkit-linear-gradient(to right, #38bdf8, white);
+background: linear-gradient(to right, white, #38bdf8);
 background-repeat: no-repeat;
 background-size: cover;
 background-position: 0 0;
