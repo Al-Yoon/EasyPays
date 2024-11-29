168b5155dbee0ce7b96f743ae4f02a9ecabc0d43
Al-Yoon <al.yoon10@gmail.com>
2024-11-28 22:14:02 -0300

    userPanelTry

--- src/views/UserPanelPrueba.jsx
+++ src/views/UserPanelPrueba.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import ModalUser from '../components/utils/Modal/ModalUser';
-import UserPic from '../components/Assets/user-avatar.svg';
-import DeleteUserButton from '../components/utils/Buttons/DeleteUserButton';
-import TicketsHistoryTable from '../components/utils/Table/TicketsHistoryTable';
+import React, { useState, useEffect, useContext } from "react";
+import ModalUser from "../components/utils/Modal/ModalUser";
+import UserPic from "../components/Assets/user-avatar.svg";
+import DeleteUserButton from "../components/utils/Buttons/DeleteUserButton";
+import TicketsHistoryTable from "../components/utils/Table/TicketsHistoryTable";
 import { AuthContext } from "../components/utils/AuthContextPrueba";
 
 const ticketsData = [
@@ -14,26 +13,64 @@ const ticketsData = [
 ];
 
 const UserPanel = () => {
-    const { user, updateUser, logout, deleteUser } = useContext(AuthContext);
+    const { user, updateUser } = useContext(AuthContext);
+    const [userData] = useState({
+        user: user.user,
+        name: user.name,
+        lastName: user.lastName,
+        mail: user.mail,
+    });
+
+    const [tempUserData, setTempUserData] = useState(userData);
+    const [open, setOpen] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
-    const navigate = useNavigate();
 
     useEffect(() => {
-        if (!user) {
-            navigate('/login');
-        }
-    }, [user, navigate]);
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/users/", {
+                    method: "GET",
+                    headers: {"Content-Type": "application/json"}
+                });
+                const jsonData = await response.json();
+                console.log(jsonData);
+            } catch (error) {
+                console.error("Error al obtener los datos del usuario:", error);
+            }
+        };
 
-    const handleUserUpdate = (updatedUser) => {
-        updateUser(updatedUser);
+        fetchData();
+    }, []);
+
+    const handleOpen = () => {
+        setOpen(true);
+        setTempUserData(userData);
     };
 
-    const handleDeleteUser = () => {
-        if (user && user.email) {
-            deleteUser(user.email);
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTempUserData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const validarMail = (mail) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+
+    const handleSave = () => {
+        if (
+            tempUserData.user &&
+            tempUserData.name &&
+            tempUserData.lastName &&
+            validarMail(tempUserData.mail) &&
+            tempUserData.pass
+        ) {
+            updateUser(tempUserData);
+            setOpen(false);
+        } else {
+            alert("Error: Verifique los campos.");
         }
-        logout();
-        navigate('/login');
     };
 
     return (
@@ -42,28 +79,41 @@ const UserPanel = () => {
                 <p className="max-w-[1240px] md:text-2xl sm:text-1xl text-xl pl-4">Mi</p>
                 <h1 className="font-bold md:text-3xl sm:text-2xl text-xl pb-3 pl-4">Usuario</h1>
                 <div className="max-w-auto mx-auto grid md:grid-cols-3 gap-8 pl-5 pr-5">
-                    
                     <div className="w-[full] h-[550px] shadow-2xl flex flex-col p-4 md:my-0 rounded-lg justify-center">
-                        <img className='w-20 mx-auto' src={UserPic} alt="/"/>
-                        <h2 className='text-2xl font-bold text-center pt-8 '>Usuario: {user.name}</h2>
-                        <div className='text-center font-medium'>
-                            <p className='py-2 my-5'>Nombre: {user.name}</p>
-                            <p className='py-2 my-5'>Apellido: {user.lastName || "Apellido"}</p>
-                            <p className='py-2 my-5'>Email: {user.email}</p>
+                        <img className="w-20 mx-auto" src={UserPic} alt="/" />
+                        <h2 className="text-2xl font-bold text-center pt-8 ">
+                            Usuario: {user.name}
+                        </h2>
+                        <div className="text-center font-medium">
+                            <p className="py-2 my-5">Nombre: {user.name}</p>
+                            <p className="py-2 my-5">
+                                Apellido: {user.lastName || "Apellido"}
+                            </p>
+                            <p className="py-2 my-5">Email: {user.email}</p>
                         </div>
                     </div>
 
                     <div className="w-full h-[30vh] shadow-2xl flex flex-col p-4 md:my-0 my-8 rounded-lg justify-center">
-                        <p className='text-center text-2xl font-bold'>Modificar Usuario</p>
-                        <button className='bg-[#299ad78d] hover:text-white w-2/3 rounded-md font-medium my-6 mx-auto px-auto py-3'>
-                            <ModalUser userData={user} onUpdateUser={handleUserUpdate}/>
+                        <p className="text-center text-2xl font-bold">Modificar Usuario</p>
+                        <button
+                            className="bg-[#299ad78d] hover:text-white w-2/3 rounded-md font-medium my-6 mx-auto px-auto py-3"
+                            onClick={handleOpen}
+                        >
+                            Editar Usuario
                         </button>
+                        <ModalUser
+                            userData={tempUserData}
+                            open={open}
+                            onClose={handleClose}
+                            onChange={handleChange}
+                            onSave={handleSave}
+                        />
                     </div>
 
                     <div className="w-full h-[30vh] shadow-2xl flex flex-col p-4 md:my-0 my-8 rounded-lg justify-center">
-                        <p className='text-center text-2xl font-bold'>Eliminar Usuario</p>
+                        <p className="text-center text-2xl font-bold">Eliminar Usuario</p>
                         <button
-                            className='bg-[#aa3d2aa4] text-[#a03a3a] hover:text-white w-2/3 rounded-md font-medium my-6 mx-auto px-6  h-[60px] font-sans uppercase pb-1'
+                            className="bg-[#aa3d2aa4] text-[#a03a3a] hover:text-white w-2/3 rounded-md font-medium my-6 mx-auto px-6  h-[60px] font-sans uppercase pb-1"
                             onClick={() => setShowDeleteModal(true)}
                         >
                             Eliminar
@@ -72,7 +122,7 @@ const UserPanel = () => {
                 </div>
                 {showDeleteModal && (
                     <DeleteUserButton
-                        onDelete={handleDeleteUser}
+                        onDelete={() => console.log("Usuario eliminado.")}
                         onCancel={() => setShowDeleteModal(false)}
                     />
                 )}
